b1e51e096fe79eedaec76d1f86f4436a86a9bdce
shokunin hiro <shokunin000@gmail.com>
2016-04-03 00:27:23 -0700

    Disabling by default
    This ends up being too costly in moments in ch. 2 & 3, and most of ch.
    4.

--- game/client/clientshadowmgr.cpp
+++ game/client/clientshadowmgr.cpp
@@ -93,7 +93,8 @@ static ConVar r_shadowrendertotexture( "r_shadowrendertotexture", "0" );
 static ConVar r_flashlight_version2( "r_flashlight_version2", "0", FCVAR_CHEAT | FCVAR_DEVELOPMENTONLY );
 
 void WorldLightCastShadowCallback(IConVar *pVar, const char *pszOldValue, float flOldValue);
-static ConVar r_worldlight_castshadows( "r_worldlight_castshadows", "1", FCVAR_CHEAT, "Allow world lights to cast shadows", true, 0, true, 1, WorldLightCastShadowCallback );
+// Disabling by default, too costly in chapter 2, 3, & 4.
+static ConVar r_worldlight_castshadows( "r_worldlight_castshadows", "0", FCVAR_CHEAT, "Allow world lights to cast shadows", true, 0, true, 1, WorldLightCastShadowCallback );
 static ConVar r_worldlight_lerptime( "r_worldlight_lerptime", "0.5", FCVAR_CHEAT );
 static ConVar r_worldlight_debug( "r_worldlight_debug", "0", FCVAR_CHEAT );
 static ConVar r_worldlight_shortenfactor( "r_worldlight_shortenfactor", "2" , FCVAR_CHEAT, "Makes shadows cast from local lights shorter" );
