b719b76f9ac6fe47b77ebcb4c24c4d2de5749ebd
vincent-t <vincentthiele@gmail.com>
2016-05-01 14:49:47 +0200

    fixed openurl implementation

--- game/shared/episodic/donation_te120.cpp
+++ game/shared/episodic/donation_te120.cpp
@@ -8,14 +8,23 @@
 #include "convar.h"
 #include "steam/steam_api.h"
 
-static void OpenURLCallback( IConVar *pConVar, const char *oldValue, float flOldValue )
+void OpenURL_f( const CCommand &args )
 {
-	ConVarRef var( pConVar );
-	const char *pURLString = var.GetString();
-
-	if (  Q_strlen( pURLString ) != 0 && (!Q_strncmp( pURLString, "http://", 7 ) || !Q_strncmp( pURLString, "https://", 8 )) )
-		steamapicontext->SteamFriends()->ActivateGameOverlayToWebPage(pURLString);
-	else
-		steamapicontext->SteamFriends()->ActivateGameOverlayToWebPage("https://www.redditdonate.com/donate/34");
+	if ( steamapicontext && steamapicontext->SteamFriends() )
+	{
+		if ( args.ArgC() < 1 | args.Arg(1) == "" )
+		{
+			//Msg("Usage: s_cl_openurl <link>\n");
+			steamapicontext->SteamFriends()->ActivateGameOverlayToWebPage("https://www.redditdonate.com/donate/34");
+			return;
+		}
+		else if ( !Q_strncmp( args.Arg(1), "http://", 7 ) || !Q_strncmp( args.Arg(1), "https://", 8 ) )
+		{
+			steamapicontext->SteamFriends()->ActivateGameOverlayToWebPage(args.Arg(1));
+			return;
+		}
+	}
+	Msg("Missing Steam API context, failed to execute: s_cl_openurl\n");
+	return;
 }
-static ConVar s_cl_openurl("s_cl_openurl", "https://www.redditdonate.com/donate/34", 0, "Open URL in Steam", OpenURLCallback );
+ConCommand s_cl_openurl( "s_cl_openurl", OpenURL_f, "Open URL in Steam Overlay.", 0 );
