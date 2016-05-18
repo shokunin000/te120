a45b9d1b56c52adc67115cffa94abd66bd234595
vincent-t <vincentthiele@gmail.com>
2016-05-18 10:14:58 +0200

    Small fixes

--- game/client/clientshadowmgr.cpp
+++ game/client/clientshadowmgr.cpp
@@ -1853,6 +1853,9 @@ ClientShadowHandle_t CClientShadowMgr::CreateProjectedTexture( ClientEntityHandl
 	if( !( flags & SHADOW_FLAGS_FLASHLIGHT ) )
 	{
 		IClientRenderable *pRenderable = ClientEntityList().GetClientRenderableFromHandle( entity );
+		if ( !pRenderable )
+			return m_Shadows.InvalidIndex();
+
 		int modelType = modelinfo->GetModelType( pRenderable->GetModel() );
 		if (modelType == mod_brush)
 		{
