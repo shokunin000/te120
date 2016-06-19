84cc0cdf85d4ce60ffc8a2acb69cde57ab2fe9cf
vincent-t <vincentthiele@gmail.com>
2016-06-19 16:12:51 +0200

    Fixed last commit

--- game/server/EnvFade.cpp
+++ game/server/EnvFade.cpp
@@ -85,24 +85,22 @@ void CEnvFade::InputFade( inputdata_t &inputdata )
 	else
 	{
 		fadeFlags |= FFADE_OUT;
+
 //TE120--
-		if ( te120_combinedlensflare.GetBool() )
+		if ( m_spawnflags & SF_FADE_DONTDIRTY )
 		{
-			if ( m_spawnflags & SF_FADE_DONTDIRTY )
-			{
-				// Hack: If SF_FADE_DONTDIRTY flag is set, disable but
-				// fade back to on.
-				CEffectData	data;
-				data.m_flScale = 0;
-				DispatchEffect( "CE_DisableDirtyLensFade", data );
-			}
-			else
-			{
-				// Otherwise turn off and stay off
-				CEffectData	data;
-				data.m_flScale = 0;
-				DispatchEffect( "CE_DisableDirtyLens", data );
-			}
+			// Hack: If SF_FADE_DONTDIRTY flag is set, disable but
+			// fade back to on.
+			CEffectData	data;
+			data.m_flScale = 0;
+			DispatchEffect( "CE_DisableDirtyLensFade", data );
+		}
+		else
+		{
+			// Otherwise turn off and stay off
+			CEffectData	data;
+			data.m_flScale = 0;
+			DispatchEffect( "CE_DisableDirtyLens", data );
 		}
 //TE120--
 	}
